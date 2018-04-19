1b59c58720d7c519355a59488fc73416d90f6987
mdagcilar <md3g11@soton.ac.uk>
2018-04-19 16:18:07 +0100

    added /test folder for JUnit testing and imported JUnit

---
+++ test/com/m3c/gp/controller/DBManagerTest.java
@@ -0,0 +1,15 @@
+package com.m3c.gp.controller;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class DBManagerTest {
+	private Connection connection;
+	
+	@Test
+	public void before() {
+		
+	}
+
+}
