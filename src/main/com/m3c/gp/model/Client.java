72b4e25eb7aa7ecf3403b25cf77c74e9fa5a6c4e
Ionut Alin Neagu <neagu_ionutalin@icloud.com>
2018-04-19 16:51:29 +0100

    client class

---
+++ src/main/com/m3c/gp/model/Client.java
@@ -0,0 +1,28 @@
+package main.com.m3c.gp.model;
+
+public class Client implements ClientInterface {
+    @Override
+    public int getClientId() {
+        return 0;
+    }
+
+    @Override
+    public void sendClientOrder(Object o) {
+
+    }
+
+    @Override
+    public void setClientBalance(double balance) {
+
+    }
+
+    @Override
+    public double getclientBalance(int clientId) {
+        return 0;
+    }
+
+    @Override
+    public void cancelClientOrder(Object o) {
+
+    }
+}
