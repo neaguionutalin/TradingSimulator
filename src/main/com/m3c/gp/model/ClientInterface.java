d7c41d193d740e20509506e122d90e7ebcc495b5
Ionut Alin Neagu <neagu_ionutalin@icloud.com>
2018-04-19 17:12:40 +0100

    new methods in class and interface

--- src/main/com/m3c/gp/model/ClientInterface.java
+++ src/main/com/m3c/gp/model/ClientInterface.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
 package main.com.m3c.gp.model;
 
 public interface ClientInterface {
@@ -5,4 +13,12 @@ public interface ClientInterface {
     void sendClientOrder(Object o);
     void setClientBalance(double balance);
     double getclientBalance(int clientId);
+    String getFirstName();
+    String getLastName();
+    String getEmail();
+    String getPassword();
+    void updateLastName(String lastname);
+    void updateFirstName(String firstname);
+    void updateEmail(String emails);
+    void updatePassword(String pass);
 }
