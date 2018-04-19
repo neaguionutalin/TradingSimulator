7eea0d15c5332c0c12bf08435096b978d3bbc193
alisaleem750 <alisaleem750@gmail.com>
2018-04-19 17:17:11 +0100

    Instrument and Instrument Test classes added

---
+++ src/main/com/m3c/gp/model/Instrument.java
@@ -0,0 +1,31 @@
+package main.com.m3c.gp.model;
+
+public class Instrument implements InstrumentInterface {
+
+	@Override
+	public InstrumentName getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setName(InstrumentName name) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setType(String type) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
---
+++ src/test/com/m3c/gp/model/InstrumentTest.java
@@ -0,0 +1,24 @@
+package test.com.m3c.gp.model;
+
+import org.junit.Test;
+
+import main.com.m3c.gp.model.Instrument;
+
+import java.sql.Connection;
+
+/**
+ * @authour: Ali Saleem
+ * @since: 19/04/18
+ * @version 1.0
+ * Instrument test
+ */
+
+public class InstrumentTest {
+	private Instrument instrument;
+	
+	@Test
+	public void before() {
+		
+	}
+
+}
