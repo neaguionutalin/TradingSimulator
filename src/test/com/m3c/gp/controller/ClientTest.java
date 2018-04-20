f32f09c0a93ce3b9be383a28feb5d5a5f2eaf674
Ionut Alin Neagu <neagu_ionutalin@icloud.com>
2018-04-20 12:04:12 +0100

    changes to clientTest

--- src/test/com/m3c/gp/controller/ClientTest.java
+++ src/test/com/m3c/gp/controller/ClientTest.java
@@ -1,17 +1,8 @@
-/*
- * Copyright (c) 2018. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
- */
+package test.com.m3c.gp.controller;
 
-package test.com.m3c.gp.controller
+import org.junit.Test;
 
-import org.junit.Before
-import org.junit.Test
-
-class ClientInterfaceTest {
+class ClientTest {
 
 
     @Test
