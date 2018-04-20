b2d4cb40aa03daabfce7ecc5d884259631f9be30
Ionut Alin Neagu <neagu_ionutalin@icloud.com>
2018-04-20 15:12:08 +0100

    tests DBManager

--- src/test/com/m3c/gp/controller/DBManagerTest.java
+++ src/test/com/m3c/gp/controller/DBManagerTest.java
@@ -1,8 +1,17 @@
 package test.com.m3c.gp.controller;
 
+import main.com.m3c.gp.controller.DBManager;
+import main.com.m3c.gp.model.*;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
 
 /**
  * Author: Metin Dagcilar
@@ -11,11 +20,47 @@ import java.sql.Connection;
  */
 
 public class DBManagerTest {
-	private Connection connection;
-	
+
 	@Test
-	public void before() {
-		
+	public void testInsertReturnOrder() {
+		DBManager dbManager = new DBManager();
+		Order order = new Order(1, new Instrument(InstrumentTicker.VOD) , 1, 50,20, OrderType.BUY);
+		dbManager.insertOrder(order);
+		Order order1 = dbManager.getOrder(1);
+		Assert.assertEquals(order,order1);
+	}
+
+	@Test
+	public void testInsertReturnCustomer() {
+		DBManager dbManager = new DBManager();
+		Client client = new Client(1, "Alin", "Neagu", "neagu_ionutalin@icloud.com", "Pa55w0rd", 10000);
+		dbManager.insertClient(client);
+		Client client1 = dbManager.getClient(1);
+		Assert.assertEquals(client,client1);
+	}
+
+	@Test
+	public void testReturnOrders(){
+		DBManager dbManager = new DBManager();
+		List<Order> orders = null;
+		orders.set(1, new Order(1, new Instrument(InstrumentTicker.VOD), 1, 50, 20, OrderType.BUY));
+		dbManager.insertOrder(orders.get(1));
+		orders.set(2, new Order(2, new Instrument(InstrumentTicker.BP) , 1, 30,50, OrderType.BUY));
+		dbManager.insertOrder(orders.get(2));
+		Assert.assertThat(orders, is(dbManager.getClientOrders(1)));
+	}
+
+	@Test
+	public void testEmailExists(){
+		DBManager dbManager = new DBManager();
+		String email = "neagu_ionutalin@icloud.com";
+		Assert.assertEquals(dbManager.checkEmailExists(email),true);
+	}
+
+	@Test
+	public void testEmailPasswordMatch(){
+		DBManager dbManager = new DBManager();
+		Assert.assertEquals(dbManager.checkEmailPasswordMatch("neagu_ionutalin@icloud.com", "pa55w0rd"), true);
 	}
 
 }
