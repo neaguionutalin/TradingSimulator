a13e41c4e3c6497f6c40c89517faa53bd9fbcb8a
mrboyles <joel@eloquenza.co.uk>
2018-04-23 14:13:45 +0100

    every little thing she does is front end

--- src/main/com/m3c/gp/beans/Login.java
+++ src/main/com/m3c/gp/beans/Login.java
@@ -1,5 +1,9 @@
 package main.com.m3c.gp.beans;
+import java.io.IOException;
+
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
@@ -12,14 +16,25 @@ public class Login {
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getPass() {
 		return pass;
 	}
+	
 	public void setPass(String pass) {
 		this.pass = pass;
+	}
+	
+	public String doLogin() {
+//		if (emailExists(email) && emailPasswordMatch(email, passs)) {
+//			return "dashboard.xhtml";
+//		} else if (emailExists(email))
+//		return null; 
+		return "dashboard.xhtml";
 	}
 	
 }
