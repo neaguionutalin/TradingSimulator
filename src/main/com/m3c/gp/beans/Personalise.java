4f3c232e6b58b2892287d53771b6bb95168df242
e-n-p <nickellam0@gmail.com>
2018-04-25 12:32:35 +0100

    you gotta fight for your right to front-end

---
+++ src/main/com/m3c/gp/beans/Personalise.java
@@ -0,0 +1,18 @@
+package main.com.m3c.gp.beans;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import main.com.m3c.gp.database.ClientDTO;
+
+public class Personalise {
+
+	public String getFullName() {
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+		ClientDTO clientDTO = (ClientDTO) session.getAttribute("client");
+		String firstName = clientDTO.getFirstname();
+		String lastName = clientDTO.getLastname();
+		return firstName + " " + lastName;
+	}
+}
