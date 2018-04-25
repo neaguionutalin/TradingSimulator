f06ca0dc1130288f1c51ab6600aa90e87852a9c4
mrboyles <joel@eloquenza.co.uk>
2018-04-25 16:12:58 +0100

    Total eclipse of the front-end

--- src/main/com/m3c/gp/beans/Portfolio.java
+++ src/main/com/m3c/gp/beans/Portfolio.java
@@ -11,13 +11,20 @@ import main.com.m3c.gp.database.Service;
 
 public class Portfolio {
 	
-	public List<OrderDTO> getOrders() {
+	public String[] getOrders() {
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
 		ClientDTO clientDTO = (ClientDTO) session.getAttribute("client");
 		int clientID = clientDTO.getClient();
 		Service service = new Service();
 		List<OrderDTO> orders = service.getClientOrders(clientID);
-		return orders;
+		String[] orderStrings = new String[orders.size()];
+		int strCnt = 0;
+		for (OrderDTO orderDTO : orders) {
+			double total = orderDTO.getQuantity()*orderDTO.getPrice();
+			String orderString = orderDTO.getType() + " " + orderDTO.getQuantity() + " " + orderDTO.getInstrument().getTicker() + " GBP" + orderDTO.getPrice()+" per Unit Total GBP" + total;
+			orderStrings[strCnt++] = orderString;
+		}
+		return orderStrings;
 	}
 }
