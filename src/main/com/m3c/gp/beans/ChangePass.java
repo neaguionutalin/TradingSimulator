30f6177b52e5d793006ecf3eeccfe3b44e8aa4fe
e-n-p <nickellam0@gmail.com>
2018-04-27 14:26:35 +0100

    One pill makes you larger, and front-end makes you small

---
+++ src/main/com/m3c/gp/beans/ChangePass.java
@@ -0,0 +1,41 @@
+package main.com.m3c.gp.beans;
+
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+import main.com.m3c.gp.database.ClientDTO;
+import main.com.m3c.gp.database.Service;
+
+@Named
+public class ChangePass {
+
+	private String oldPass;
+	private String newPassFirst;
+	private String newPassSecond;
+	
+	public void setOldPass(String oldPass) {
+		this.oldPass = oldPass;
+	}
+	
+	public void setNewPassFirst(String newPassFirst) {
+		this.newPassFirst = newPassFirst;
+	}
+	
+	public void setNewPassSecond(String newPassSecond) {
+		this.newPassSecond = newPassSecond;
+	}
+	
+	public String doChangePass() {
+		if (newPassFirst.equals(newPassSecond)) {
+			Service service = new Service();
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+			ClientDTO clientDTO = (ClientDTO) session.getAttribute("client");
+			String email = clientDTO.getEmail();
+			service.updatePassword(email, oldPass, newPassFirst);
+			return "settings?faces-redirect=true";
+		}
+		return null;
+	}
+}
